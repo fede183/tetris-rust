2af71872ed03d4d4f2cf5f869d0d4cba892faee5
fede183 <fede.183@hotmail.com>
2024-03-23 11:23:29 -0300

    board position

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,9 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
-use crate::classes::config::{DISPLAY_BOARD_WIDTH, DISPLAY_BOARD_HEIGTH};
+use crate::classes::config::{DISPLAY_BOARD_WIDTH, DISPLAY_BOARD_HEIGTH, DISPLAY_BOARD_X, DISPLAY_BOARD_Y, DISPLAY_NEXT_PIECE_BLOCK_WIDTH, DISPLAY_NEXT_PIECE_BLOCK_HEIGTH, 
+DISPLAY_NEXT_PIECE_BLOCK_POSITION_X, DISPLAY_NEXT_PIECE_BLOCK_POSITION_Y};
+
 
 pub fn setup(
     mut commands: Commands,
@@ -11,17 +13,31 @@ pub fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    let shape = Mesh2dHandle(meshes.add(Rectangle::new(DISPLAY_BOARD_WIDTH, DISPLAY_BOARD_HEIGTH)));
-
     let color = Color::BLACK;
 
+    let board_shape = Mesh2dHandle(meshes.add(Rectangle::new(DISPLAY_BOARD_WIDTH, DISPLAY_BOARD_HEIGTH)));
+
     commands.spawn(MaterialMesh2dBundle {
-        mesh: shape,
+        mesh: board_shape,
         material: materials.add(color),
         transform: Transform::from_xyz(
             // Distribute shapes from -X_EXTENT to +X_EXTENT.
+            DISPLAY_BOARD_X,
+            DISPLAY_BOARD_Y,
             0.0,
-            0.0,
+        ),
+        ..default()
+    });
+
+    let next_piece_block_shape = Mesh2dHandle(meshes.add(Rectangle::new(DISPLAY_NEXT_PIECE_BLOCK_WIDTH, DISPLAY_NEXT_PIECE_BLOCK_HEIGTH)));
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: next_piece_block_shape,
+        material: materials.add(color),
+        transform: Transform::from_xyz(
+            // Distribute shapes from -X_EXTENT to +X_EXTENT.
+            DISPLAY_NEXT_PIECE_BLOCK_POSITION_X,
+            DISPLAY_NEXT_PIECE_BLOCK_POSITION_Y,
             0.0,
         ),
         ..default()
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,9 @@ use bevy::prelude::*;
 mod app;
 mod classes;
 
-use crate::classes::config::{DISPLAY_WIDTH, DISPLAY_HEIGTH};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: (DISPLAY_WIDTH, DISPLAY_HEIGTH).into(),
-                ..default()
-            }),
-            ..default()
-        }))
         .insert_resource(ClearColor(Color::ANTIQUE_WHITE))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, app::setup)
