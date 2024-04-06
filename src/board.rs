cbde5add3304a312f7e4c473bcb9018520938728
fede183 <fede.183@hotmail.com>
2024-04-06 16:32:15 -0300

    border

--- src/board.rs
+++ src/board.rs
@@ -6,19 +6,39 @@ pub fn setup_board(mut commands: Commands) {
     commands.spawn(SpriteBundle {
         transform: Transform {
             translation: Vec3 {
-                x: 0.0,
-                y: 0.0,
-                ..default()
+                x: 0.,
+                y: 0.,
+                z: 1.,
             },
             scale: Vec3 {
                 x: DISPLAY_BOARD_WIGTH,
                 y: DISPLAY_BOARD_HEIGHT,
-                z: 0.0,
+                z: 1.,
             },
             ..default()
         },
         sprite: Sprite {
             color: BOARD_COLOR,
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        transform: Transform {
+            translation: Vec3 {
+                x: 0.,
+                y: 0.,
+                z: 0.,
+            },
+            scale: Vec3 {
+                x: DISPLAY_BORDER_WIGTH,
+                y: DISPLAY_BORDER_HEIGHT,
+                z: 0.,
+            },
+            ..default()
+        },
+        sprite: Sprite {
+            color: BORDER_COLOR,
             ..default()
         },
         ..default()
