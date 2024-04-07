8ff058bdde4ed92dec1ac33fec6b3268d6b13e65
fede183 <fede.183@hotmail.com>
2024-04-06 23:37:33 -0300

    border

--- src/board.rs
+++ src/board.rs
@@ -1,46 +1,8 @@
 use bevy::prelude::*;
 use crate::config::*;
+use crate::common::draw_rectable;
 
 
-pub fn setup_board(mut commands: Commands) {
-    commands.spawn(SpriteBundle {
-        transform: Transform {
-            translation: Vec3 {
-                x: 0.,
-                y: 0.,
-                z: 1.,
-            },
-            scale: Vec3 {
-                x: DISPLAY_BOARD_WIGTH,
-                y: DISPLAY_BOARD_HEIGHT,
-                z: 1.,
-            },
-            ..default()
-        },
-        sprite: Sprite {
-            color: BOARD_COLOR,
-            ..default()
-        },
-        ..default()
-    });
-    commands.spawn(SpriteBundle {
-        transform: Transform {
-            translation: Vec3 {
-                x: 0.,
-                y: 0.,
-                z: 0.,
-            },
-            scale: Vec3 {
-                x: DISPLAY_BORDER_WIGTH,
-                y: DISPLAY_BORDER_HEIGHT,
-                z: 0.,
-            },
-            ..default()
-        },
-        sprite: Sprite {
-            color: BORDER_COLOR,
-            ..default()
-        },
-        ..default()
-    });
+pub fn setup_board(commands: Commands) {
+    draw_rectable(commands, 0., 0., DISPLAY_BOARD_HEIGHT, DISPLAY_BOARD_WIGTH, BORDER_SIZE, BOARD_COLOR, BORDER_COLOR);
 }
--- src/common.rs
+++ src/common.rs
@@ -10,3 +10,47 @@ pub fn toggle_resolution(mut windows: Query<&mut Window>) {
 pub fn init_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
+
+pub fn draw_rectable(mut commands: Commands, x_position: f32, y_position: f32, height: f32, wigth: f32, border_size: f32, 
+                     fill_color: Color, border_color: Color) {
+    commands.spawn(SpriteBundle {
+        transform: Transform {
+            translation: Vec3 {
+                x: x_position,
+                y: y_position,
+                z: 1.,
+            },
+            scale: Vec3 {
+                x: wigth,
+                y: height,
+                z: 1.,
+            },
+            ..default()
+        },
+        sprite: Sprite {
+            color: fill_color,
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn(SpriteBundle {
+        transform: Transform {
+            translation: Vec3 {
+                x: x_position,
+                y: y_position,
+                z: 0.,
+            },
+            scale: Vec3 {
+                x: wigth + border_size,
+                y: height + border_size,
+                z: 0.,
+            },
+            ..default()
+        },
+        sprite: Sprite {
+            color: border_color,
+            ..default()
+        },
+        ..default()
+    });
+}
