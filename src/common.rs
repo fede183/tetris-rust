54504c5ecf9ad1fd1746a4a76d9e6232d501e6c1
fede183 <fede.183@hotmail.com>
2024-04-13 12:10:56 -0300

    piece draw

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use crate::config::*;
+use crate::{config::*, Piece, PointOnBoard};
 
 pub fn toggle_resolution(mut windows: Query<&mut Window>) {
     let mut window = windows.single_mut();
@@ -11,7 +11,7 @@ pub fn init_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-pub fn draw_rectable(commands: &mut Commands, positions: Vec3, height: f32, wigth: f32, color: Color) {
+pub fn draw_rectangle(commands: &mut Commands, positions: Vec3, height: f32, wigth: f32, color: Color) {
     commands.spawn(SpriteBundle {
         transform: Transform {
             translation: positions,
@@ -30,8 +30,39 @@ pub fn draw_rectable(commands: &mut Commands, positions: Vec3, height: f32, wigt
     });
 }
 
-pub fn draw_rectable_with_border(commands: &mut Commands, positions: Vec3, height: f32, wigth: f32, border_size: f32, fill_color: Color, border_color: Color) {
-    draw_rectable(commands, Vec3 { z: positions.z + 1., ..positions }, height, wigth, fill_color);
+pub fn draw_rectangle_with_border(commands: &mut Commands, positions: Vec3, height: f32, wigth: f32, border_size: f32, fill_color: Color, border_color: Color) {
+    draw_rectangle(commands, Vec3 { z: positions.z + 1., ..positions }, height, wigth, fill_color);
 
-    draw_rectable(commands, positions, height + border_size, wigth + border_size, border_color);
+    draw_rectangle(commands, positions, height + border_size, wigth + border_size, border_color);
+}
+
+fn draw_point_in_next_piece(commands: &mut Commands, point: &PointOnBoard) {
+    let x_position = point.x as f32 * SQUARE_SIZE;
+    let y_position = point.y as f32 * SQUARE_SIZE;
+    draw_rectangle(commands, Vec3{ x: DISPLAY_NEXT_PIECE_POSITION_X + x_position, y: DISPLAY_NEXT_PIECE_POSITION_Y + y_position, z: 1.}, SQUARE_SIZE, SQUARE_SIZE, point.color);
+}
+
+pub fn draw_next_piece(commands: &mut Commands, piece: &Piece) {
+    for point in &piece.0 {
+        draw_point_in_next_piece(commands, &point);
+    }
+}
+
+pub fn draw_cell_on_board(commands: &mut Commands, x: i32, y: i32) {
+    let x_position = SQUARE_SIZE* (x as f32);
+    let y_position = SQUARE_SIZE* (y as f32);
+    let square_size = SQUARE_SIZE - 5.;
+    draw_rectangle_with_border(commands, Vec3{ x: -DISPLAY_FIRST_BOARD_POSITION_X + x_position, y: DISPLAY_FIRST_BOARD_POSITION_Y - y_position, z: 1.}, square_size, square_size, 5., BOARD_COLOR, BORDER_SQUARE_COLOR);
+}
+
+fn draw_point_in_board(commands: &mut Commands, point: &PointOnBoard) {
+    let x_position = point.x as f32 * SQUARE_SIZE;
+    let y_position = point.y as f32 * SQUARE_SIZE;
+    draw_rectangle(commands, Vec3{ x: -DISPLAY_FIRST_BOARD_POSITION_X + x_position, y: DISPLAY_FIRST_BOARD_POSITION_Y - y_position, z: 2.}, SQUARE_SIZE, SQUARE_SIZE, point.color);
+}
+
+pub fn draw_piece_on_board(commands: &mut Commands, piece: &Piece) {
+    for point in &piece.0 {
+        draw_point_in_board(commands, &point);
+    }
 }
---
+++ src/score.rsr
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+use crate::config::*;
+
+#[derive(Component)]
+pub struct ScoreAndLines {
+    pub score: i32,
+    pub lines: i32,
+}
+
+pub fn init_score(mut commands: Commands) {
+    commands.spawn(ScoreAndLines {score:0, lines:0});
+}
+
+pub fn setup_score(mut commands: Commands, asset_server: Res<AssetServer>, query: Query<&ScoreAndLines>) {
+    let font = asset_server.load("fonts/textFont.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: SCORE_FONT_SIZE,
+        color: SCORE_COLOR,
+    };
+    let text_justification = JustifyText::Center;
+    for score in &query {
+        let score_text = "Score: ".to_owned() + &score.score.to_string();
+        let lines_text = "Lines: ".to_owned() + &score.lines.to_string();
+
+        commands.spawn((
+            Text2dBundle {
+                text: Text::from_section(score_text, text_style.clone())
+                    .with_justify(text_justification),
+                transform: Transform::from_xyz(-DISPLAY_SCORE_POSITION_X, DISPLAY_SCORE_POSITION_Y, 0.),
+                ..default()
+            },
+        ));
+        commands.spawn((
+            Text2dBundle {
+                text: Text::from_section(lines_text, text_style.clone())
+                    .with_justify(text_justification),
+                transform: Transform::from_xyz(-DISPLAY_SCORE_POSITION_X, DISPLAY_LINES_POSITION_Y, 0.),
+                ..default()
+            },
+        ));
+    }
+}
