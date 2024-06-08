d42f45088fd3957b816233a1afbc61aa3aeecf87
fede183 <fede.183@hotmail.com>
2024-06-08 15:06:08 -0300

    fix despawn

--- src/events.rs
+++ src/events.rs
@@ -8,33 +8,33 @@ pub fn piece_input_system(
     mut commands: Commands,
     input: Res<ButtonInput<KeyCode>>,
     mut game_data: ResMut<GameData>,
-    mut query: Query<(Entity, &mut Transform), With<BoardPieceComponent>>,
+    mut query_piece_transformation: Query<(Entity, &mut Transform), With<BoardPieceComponent>>,
     ) {
-     let (entity, mut transform) = query.single_mut();
+    let (entity, mut transform) = query_piece_transformation.single_mut();
 
-     if input.just_pressed(KeyCode::ArrowDown) {
+    if input.just_pressed(KeyCode::ArrowDown) {
          if game_data.descend() {
              transform.translation.y -= SQUARE_SIZE;
          } else {
+            commands.entity(entity).despawn_recursive();
             spawn_piece(&mut commands, &game_data);
-            commands.entity(entity).despawn();
          }
-     }
-     if input.just_pressed(KeyCode::ArrowLeft) {
+    }
+    if input.just_pressed(KeyCode::ArrowLeft) {
          if game_data.move_left() {
              transform.translation.x -= SQUARE_SIZE;
          }
-     }
-     if input.just_pressed(KeyCode::ArrowRight) {
+    }
+    if input.just_pressed(KeyCode::ArrowRight) {
          if game_data.move_right() {
              transform.translation.x += SQUARE_SIZE;
          }
-     }
-     if input.just_pressed(KeyCode::Space) {
+    }
+    if input.just_pressed(KeyCode::Space) {
          if game_data.rotate() {
             let rotation90 = Quat::from_rotation_z((90.0_f32).to_radians());
             
             transform.rotate(rotation90);
          }
-     }
+    }
 }
